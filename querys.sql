4c88660f122b9545794f6f5ae05383e85c7fe19b
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-12-03 15:26:25 +0100

    Requêtes num 3et4

--- querys.sql
+++ querys.sql
@@ -7,7 +7,13 @@ insert into advisors(full_name,email) values ('khalid bdjfh','khalid@gmail.com')
 update customers set phone = 0645454545 where customer_id = 2;
 
 
+------------- 3-------------------------
 
 
+select * from customers;
 
+
+------------- 4-------------------------
+
+select full_name,email from customers;
 
