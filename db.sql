5ad8503dd9061b9062fec8429e7584a3f9698c5d
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-12-03 14:46:51 +0100

    table accounts et leur value

--- db.sql
+++ db.sql
@@ -33,3 +33,23 @@ INSERT INTO customers (full_name, email, phone) VALUES
 ('reda bd','rada@gmail.com',602124578),
 ('aya aya','aya@gmail.com',649780215),
 ('farh','farh@gmail.com',789250102);
+
+
+
+CREATE TABLE accounts (
+  account_id INT AUTO_INCREMENT PRIMARY KEY,
+  account_number DECIMAL(10,0),
+  balance DECIMAL(10,0),
+  account_type ENUM('Checking','Savings','Business'),
+  customerid INT,
+  advisorid INT,
+  FOREIGN KEY (customerid) REFERENCES customers(customer_id),
+  FOREIGN KEY (advisorid) REFERENCES advisors(advisor_id)
+);
+
+INSERT INTO accounts (account_number, balance, account_type, customerid, advisorid) VALUES
+(12035,15000,'Savings',1,1),
+(12548,15996,'Business',2,2),
+(56985,200,'Checking',3,3),
+(65332,2300,'Savings',4,4),
+(56025,500,'Savings',5,5);
