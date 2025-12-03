d53baf682b7e136e410b1e389e210a9835275e83
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-12-03 14:50:32 +0100

    ajoute fichier querys avec les deux premier Requêtes

---
+++ querys.sql
@@ -0,0 +1,13 @@
+------------- 1-------------------------
+
+insert into advisors(full_name,email) values ('khalid bdjfh','khalid@gmail.com');
+
+------------- 2-------------------------
+
+update customers set phone = 0645454545 where customer_id = 2;
+
+
+
+
+
+
