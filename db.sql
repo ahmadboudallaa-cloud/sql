48bbaaba6c30b7dc7047a49cd1b89c5e39dde77f
Ahmed Boudallaa <ahmadboudallaa@gmail.com>
2025-12-03 14:46:06 +0100

    table customers et leur value

--- db.sql
+++ db.sql
@@ -16,3 +16,20 @@ INSERT INTO advisors (full_name, email) VALUES
 ('hanae','hanae@gmail.com'),
 ('amine','amine@gmail.com'),
 ('sanae','sanae@gmail.com');
+
+
+
+CREATE TABLE customers (
+  customer_id INT AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(250),
+  email VARCHAR(250),
+  phone DECIMAL(10,0),
+  registration_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO customers (full_name, email, phone) VALUES
+('ahmed boudallaa','ahmadbd05@gmail.com',706809988),
+('ayoub bdhbf','ayoub@gmail.com',677889966),
+('reda bd','rada@gmail.com',602124578),
+('aya aya','aya@gmail.com',649780215),
+('farh','farh@gmail.com',789250102);
